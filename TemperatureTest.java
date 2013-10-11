744113c2ed9d93c667d7849d3051c22595b69eb4
Kaichen Wang <kaichen.wang@outlook.com>
2013-10-11 15:02:44 -0400

    Created testChangeUnits test case and added class variables

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -9,24 +9,52 @@ import static org.junit.Assert.* ;
 
 public class TemperatureTest {
 
-  // Add multiple tests to check all functions of
-  // {@Code Temperature} class.
-  
-  @Test
-  public void testGetValue () {
-	  double desiredValue = 15;
-	  Temperature.Units desiredUnit = Temperature.Units.KELVIN;
-	  Temperature testTemperature = new Temperature (desiredValue, desiredUnit);
-	  double getvalueResult = testTemperature.getValue();
-	  assertEquals(desiredValue, getvalueResult, .1);
-  }
-  
-  @Test
-  public void testGetUnits () {
-	  double desiredValue = 15;
-	  Temperature.Units desiredUnit = Temperature.Units.KELVIN;
-	  Temperature testTemperature = new Temperature (desiredValue, desiredUnit);
-	  Temperature.Units getUnitsResult = testTemperature.getUnits();
-	  assertEquals(desiredUnit, getUnitsResult);
-  }
+	// Add multiple tests to check all functions of
+	// {@Code Temperature} class.
+
+	private Temperature temperature;
+	private double testValue;
+	private double resultValue;
+	private Temperature.Units testUnit;
+	private Temperature.Units conversionTestUnit;
+	private Temperature.Units resultUnit;
+
+	final double ERROR = 0.1;
+
+	@Before
+	public void setUp () throws Exception {	
+		System.out.println("-TEST");
+	}
+
+	//Tests for getValue
+	@Test
+	public void testGetValue1 () {
+		this.testValue = 15;
+		this.testUnit = Temperature.Units.KELVIN;
+		this.temperature = new Temperature (this.testValue, this.testUnit);
+		this.resultValue = this.temperature.getValue ();
+		assertEquals (this.testValue, this.resultValue, ERROR);
+	}
+
+	//Tests for getUnits
+	@Test
+	public void testGetUnits1 () {
+		this.testValue = 15;
+		this.testUnit = Temperature.Units.FAHRENHEIT;
+		this.temperature = new Temperature (this.testValue, this.testUnit);
+		this.resultUnit = this.temperature.getUnits();
+		assertEquals(this.testUnit, this.resultUnit);
+	}
+
+	//Tests for changeUnits
+	@Test
+	public void testChangeUnits1 () {
+		this.testValue = 0;
+		this.testUnit = Temperature.Units.FAHRENHEIT;
+		this.conversionTestUnit = Temperature.Units.CELSIUS;
+		this.temperature = new Temperature (this.testValue, this.testUnit);
+		this.temperature.changeUnits(conversionTestUnit);
+		this.resultUnit = this.temperature.getUnits();
+		assertEquals(this.conversionTestUnit, this.resultUnit);
+	}
 }
